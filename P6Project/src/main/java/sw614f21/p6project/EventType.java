25d4818620fa4d2a0fe795f7895526142d7354b1
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-03-17 14:56:03 +0100

    removed comment

--- P6Project/src/main/java/sw614f21/p6project/EventType.java
+++ P6Project/src/main/java/sw614f21/p6project/EventType.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sw614f21.p6project;
 
-/**
- *
- * @author Michael
- */
 public enum EventType {
     HeatPumpOn, PowerPlugsDiningRoomOn, PowerPlugsAtticOn, PowerPlugsMasterBedroomOn, PowerPlugsBedroom2On, PowerPlugsBedroom3On, 
     PowerPlugsBedroom4On, PowerClothesWasherOn, DishwasherOn, FirstFloorLightsOn, SecondFloorLightsOn, ClothesWasherOn, DryerOn, 
