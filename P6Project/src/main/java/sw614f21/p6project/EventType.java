1dc4ae1d324ed6d2547ed6536fe38c9c28cc4761
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-03-17 14:49:56 +0100

    EvenType enum created

---
+++ P6Project/src/main/java/sw614f21/p6project/EventType.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sw614f21.p6project;
+
+/**
+ *
+ * @author Michael
+ */
+public enum EventType {
+    HeatPumpOn, PowerPlugsDiningRoomOn, PowerPlugsAtticOn, PowerPlugsMasterBedroomOn, PowerPlugsBedroom2On, PowerPlugsBedroom3On, PowerPlugsBedroom4On,
+    PowerClothesWasherOn, DishwasherOn, FirstFloorLightsOn, SecondFloorLightsOn, ClothesWasherOn, DryerOn, Bedroom3LaptopOn,
+    DesktopPCMonitorOn, FanOn, HeatingPadOn, LivingRoomBlueRayOn, LivingRoomTVOn, MasterBedroomBlueRayOn,
+    MasterBedroomTVOn, SlowCookerOn, VacuumOn, VideoGameOn , Bedroom4LightsOn, MasterBedroomLightsOn, Bathroom2LightsOn,
+    Bedroom2LightsOn, Bathroom1LightsOn, Bedroom3LightsOn,  ParentBDownstairs, ParentAUpstairs, KitchenLightsAOn, KitchenLightsBOn,
+    LightsBasementOn , LightsAtticOn, OvenOn , Bedroom2LaptopOn , RooftopWindSpeedWindy, ChildAUpstairs, ChildBUpstairs,
+    ChildBDownstairs, ChildADownstairs
+}
