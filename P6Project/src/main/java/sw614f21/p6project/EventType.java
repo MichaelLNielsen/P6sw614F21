97ce7244233113d7121afa37979c1cf336ec9f01
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-03-17 14:51:06 +0100

    EvenType enum updated

--- P6Project/src/main/java/sw614f21/p6project/EventType.java
+++ P6Project/src/main/java/sw614f21/p6project/EventType.java
@@ -10,9 +10,9 @@ package sw614f21.p6project;
  * @author Michael
  */
 public enum EventType {
-    HeatPumpOn, PowerPlugsDiningRoomOn, PowerPlugsAtticOn, PowerPlugsMasterBedroomOn, PowerPlugsBedroom2On, PowerPlugsBedroom3On, PowerPlugsBedroom4On,
-    PowerClothesWasherOn, DishwasherOn, FirstFloorLightsOn, SecondFloorLightsOn, ClothesWasherOn, DryerOn, Bedroom3LaptopOn,
-    DesktopPCMonitorOn, FanOn, HeatingPadOn, LivingRoomBlueRayOn, LivingRoomTVOn, MasterBedroomBlueRayOn,
+    HeatPumpOn, PowerPlugsDiningRoomOn, PowerPlugsAtticOn, PowerPlugsMasterBedroomOn, PowerPlugsBedroom2On, PowerPlugsBedroom3On, 
+    PowerPlugsBedroom4On, PowerClothesWasherOn, DishwasherOn, FirstFloorLightsOn, SecondFloorLightsOn, ClothesWasherOn, DryerOn, 
+    Bedroom3LaptopOn, DesktopPCMonitorOn, FanOn, HeatingPadOn, LivingRoomBlueRayOn, LivingRoomTVOn, MasterBedroomBlueRayOn,
     MasterBedroomTVOn, SlowCookerOn, VacuumOn, VideoGameOn , Bedroom4LightsOn, MasterBedroomLightsOn, Bathroom2LightsOn,
     Bedroom2LightsOn, Bathroom1LightsOn, Bedroom3LightsOn,  ParentBDownstairs, ParentAUpstairs, KitchenLightsAOn, KitchenLightsBOn,
     LightsBasementOn , LightsAtticOn, OvenOn , Bedroom2LaptopOn , RooftopWindSpeedWindy, ChildAUpstairs, ChildBUpstairs,
