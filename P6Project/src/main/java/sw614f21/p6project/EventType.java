1c2eee7d4eee9ea00aaddb9eeb71a9af06be76b9
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-03-19 13:50:31 +0100

    Added endpoints

--- P6Project/src/main/java/sw614f21/p6project/EventType.java
+++ P6Project/src/main/java/sw614f21/p6project/EventType.java
@@ -1,8 +1,8 @@
 package sw614f21.p6project;
 
 public enum EventType {
-    HeatPumpOn(7.0), PowerPlugsDiningRoomOn(0.0), PowerPlugsAtticOn(0.0), PowerPlugsMasterBedroomOn(0.0), PowerPlugsBedroom2On(23.0), PowerPlugsBedroom3On(0.0),
-    PowerPlugsBedroom4On(0.0), PowerClothesWasherOn(0.0), DishwasherOn(0.0), FirstFloorLightsOn(0.0), SecondFloorLightsOn(0.0), ClothesWasherOn(3.0), DryerOn(0.0),
+    HeatPumpOn(7.0), PowerPlugsDiningRoomOn(0.0), PowerPlugsAtticOn(0.0), PowerPlugsMasterBedroomOn(0.0), PowerPlugsBedroom2On(23.0), PowerPlugsBedroom4On(0.0), PowerPlugsBedroom3On(0.0),
+    PowerClothesWasherOn(0.0), DishwasherOn(0.0), FirstFloorLightsOn(0.0), SecondFloorLightsOn(0.0), ClothesWasherOn(3.0), DryerOn(0.0),
     Bedroom3LaptopOn(0.0), DesktopPCMonitorOn(0.0), FanOn(0.0), HeatingPadOn(0.0), LivingRoomBlueRayOn(0.0), LivingRoomTVOn(0.0), MasterBedroomBlueRayOn(0.0),
     MasterBedroomTVOn(0.0), SlowCookerOn(0.0), VacuumOn(0.0), VideoGameOn(0.0) , Bedroom4LightsOn(0.0), MasterBedroomLightsOn(0.0), Bathroom2LightsOn(0.0),
     Bedroom2LightsOn(0.0), Bathroom1LightsOn(0.0), Bedroom3LightsOn(0.0),  ParentBDownstairs(0.0), ParentAUpstairs(0.0), KitchenLightsAOn(0.0), KitchenLightsBOn(0.0),
