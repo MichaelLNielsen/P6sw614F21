605c07b7a5613991fd9be7629ccd7a546be49dbc
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-03-23 09:05:12 +0100

    removed print

---
+++ P6Project/src/main/java/sw614f21/p6project/Endpoint.java
@@ -0,0 +1,37 @@
+package sw614f21.p6project;
+
+/**
+ *
+ * @author Michael
+ */
+public class Endpoint extends Symbol implements Comparable<Endpoint>{
+    public Integer Timestamp;
+    public Integer OccurrenceID;
+    public boolean Start;
+    
+    
+    public Endpoint (EventType symbol, int timestamp, boolean start, int occurrenceID){
+        SymbolID = symbol;
+        Timestamp = timestamp;
+        Start = start;
+        OccurrenceID = occurrenceID;
+    }
+
+    @Override
+    public int compareTo(Endpoint o) {
+        int evaluation = Timestamp.compareTo(o.Timestamp);
+        if (evaluation == 0){
+            int evaluation2 = SymbolID.compareTo(o.SymbolID);
+            if (evaluation2 == 0){
+                return OccurrenceID.compareTo(o.OccurrenceID);
+            }
+            return evaluation2;
+        }
+        return evaluation;
+    }
+    @Override
+    public String toString(){
+        String output = SymbolID.toString() + " " + (Start ? '+' :  '-') + " " + Timestamp.toString();
+        return output;
+    }
+}
