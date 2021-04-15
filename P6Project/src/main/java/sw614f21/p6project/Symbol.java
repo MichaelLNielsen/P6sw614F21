840718baaa51c4f6993abc0bd76c950eec94650f
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-04-15 11:25:15 +0200

    Benchmarking activated

--- P6Project/src/main/java/sw614f21/p6project/Symbol.java
+++ P6Project/src/main/java/sw614f21/p6project/Symbol.java
@@ -1,11 +1,5 @@
 package sw614f21.p6project;
 
 public abstract class Symbol {
-    public EventType SymbolID;
     public String EventID;
-    
-    public void SetSymbolID(EventType symbol){
-        SymbolID = symbol;
-        EventID = symbol.toString();
-    }
 }
