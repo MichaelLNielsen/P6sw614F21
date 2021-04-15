33fb8824722cf5dc052f17bc0c4e5357f1cb73c4
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-04-15 10:17:23 +0200

    Changed Events to strings instead of eventtype

--- P6Project/src/main/java/sw614f21/p6project/ClusterSymbol.java
+++ P6Project/src/main/java/sw614f21/p6project/ClusterSymbol.java
@@ -9,19 +9,19 @@ public class ClusterSymbol extends PatternSymbol{
     public double Mean;
     public double Deviation;
     public ArrayList<ClusterElement> ClusterElements;
-    public ClusterSymbol(EventType symbolID, boolean start, double mean, double deviation){
+    public ClusterSymbol(String symbolID, boolean start, double mean, double deviation){
         super(symbolID, start);
         Mean = mean;
         Deviation = deviation;
         
     }
-    public ClusterSymbol( EventType symbolID, boolean start){
+    public ClusterSymbol( String symbolID, boolean start){
         super(symbolID, start);
 
     }
     
     @Override
     public String toString() {
-        return SymbolID.toString().concat((Start ? "+" : "-").concat(" Mean: ").concat(doubleFormat.format(Mean)).concat(" Deviation: ").concat(doubleFormat.format(Deviation)));
+        return EventID.concat((Start ? "+" : "-").concat(" Mean: ").concat(doubleFormat.format(Mean)).concat(" Deviation: ").concat(doubleFormat.format(Deviation)));
     }
 }
