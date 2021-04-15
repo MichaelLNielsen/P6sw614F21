a517b9b23f6ceb34d1c6501523b2d11352ba27af
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-04-15 10:16:45 +0200

    Changed the settings

--- P6Project/src/main/java/sw614f21/p6project/Program.java
+++ P6Project/src/main/java/sw614f21/p6project/Program.java
@@ -5,22 +5,22 @@ import java.util.Collections;
 
 public class Program {
     public static void main(String[] args) throws IOException{
-        
-        //TPMiner tpMiner = new TPMiner();
-//        CulturedMiner cultMiner = new CulturedMiner();
-//
-//        ArrayList<ClusterPattern> patterns = cultMiner.CultureMine(100, 1, 1);
-////        ArrayList<TemporalPattern> patterns = tpMiner.TPMine(2);
-//        System.out.println(patterns.size());
-//        for (int i = 0; i < patterns.size(); i++){
-//           System.out.println("Pattern = " + patterns.get(i).Pattern );
-//        }
+   
+        TPMiner tpMiner = new TPMiner();
+        CulturedMiner cultMiner = new CulturedMiner();
 
-        ArrayList<OccurrenceSequence> output = CSVReader.GetBenchmarkSequences();
-        for (OccurrenceSequence seq : output) {
-            System.out.println(seq.Sequence);
+        ArrayList<ClusterPattern> patterns = cultMiner.CultureMine(500, 3, 3);
+//        ArrayList<TemporalPattern> patterns = tpMiner.TPMine(2);
+        System.out.println(patterns.size());
+        for (int i = 0; i < patterns.size(); i++){
+           System.out.println("Pattern = " + patterns.get(i).Pattern );
         }
-        System.out.println("Number of days: " + output.size());
+
+//        ArrayList<OccurrenceSequence> output = CSVReader.GetBenchmarkSequences();
+//        for (OccurrenceSequence seq : output) {
+//            System.out.println(seq.Sequence);
+//        }
+//        System.out.println("Number of days: " + output.size());
     }
 
 
