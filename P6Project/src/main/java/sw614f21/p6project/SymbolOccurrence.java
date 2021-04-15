bca060d48c14028a13b035ec0562065448ce449a
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-04-15 11:25:04 +0200

    Benchmarking activated

--- P6Project/src/main/java/sw614f21/p6project/SymbolOccurrence.java
+++ P6Project/src/main/java/sw614f21/p6project/SymbolOccurrence.java
@@ -5,11 +5,6 @@ public class SymbolOccurrence extends Symbol {
     public int StartingTime;
     public int FinishingTime;
 
-    public SymbolOccurrence (EventType SymbolID, int StartingTime){
-        //this.SymbolID = SymbolID;
-        SetSymbolID(SymbolID);
-        this.StartingTime = StartingTime;
-    }
 
     public SymbolOccurrence (String SymbolID, int StartingTime){
         this.EventID = SymbolID;
