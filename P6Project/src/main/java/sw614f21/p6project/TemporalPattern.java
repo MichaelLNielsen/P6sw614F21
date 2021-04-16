f5248c6cd0ef305645b799f8f1852e39256bdd59
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-04-16 13:36:00 +0200

    Tostring on patterns

--- P6Project/src/main/java/sw614f21/p6project/TemporalPattern.java
+++ P6Project/src/main/java/sw614f21/p6project/TemporalPattern.java
@@ -9,6 +9,27 @@ public class TemporalPattern {
     public TemporalPattern(ArrayList<PatternSymbol> tPattern) {
         TPattern = tPattern;
     }
-
+    
+    @Override
+    public String toString(){
+        String wholeString = "";
+        int i;
+        for (i = 0; i < TPattern.size() - 1; i++){
+            if (TPattern.get(i).SameTimeAsPrevious == false && TPattern.get(i+1).SameTimeAsPrevious){
+                wholeString = wholeString.concat("(");
+            }
+            wholeString = wholeString.concat(TPattern.get(i).toString());
+            if (TPattern.get(i).SameTimeAsPrevious == true && TPattern.get(i+1).SameTimeAsPrevious == false){
+                wholeString = wholeString.concat(")");
+            }
+        }
+        
+        wholeString = wholeString.concat(TPattern.get(i).toString());
+        if (TPattern.get(i).SameTimeAsPrevious){
+            wholeString = wholeString.concat(")");
+        }
+        
+        return wholeString;
+    }
 
 }
