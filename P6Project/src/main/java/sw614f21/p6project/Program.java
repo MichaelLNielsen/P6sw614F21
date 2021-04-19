824e12ade0cc85e763f5df440aecffe3dc1e64f8
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-04-19 10:17:02 +0200

    made it easy to add new houses to benchmark data

--- P6Project/src/main/java/sw614f21/p6project/Program.java
+++ P6Project/src/main/java/sw614f21/p6project/Program.java
@@ -5,12 +5,12 @@ import java.util.Collections;
 
 public class Program {
     public static void main(String[] args) throws IOException{
-   
+    
         TPMiner tpMiner = new TPMiner();
 //        CulturedMiner cultMiner = new CulturedMiner();
 //
 //        ArrayList<ClusterPattern> patterns = cultMiner.CultureMine(1400, 86400, 800);
-        ArrayList<TemporalPattern> patterns = tpMiner.TPMine(2);
+        ArrayList<TemporalPattern> patterns = tpMiner.TPMine(2800);
 //          Random Commentar
         System.out.println(patterns.size());
         for (int i = 0; i < patterns.size(); i++){
