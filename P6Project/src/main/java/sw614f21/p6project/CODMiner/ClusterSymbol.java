b500936eefe80e2a448fe9f8674e0ff7ce8a9f06
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2021-05-14 14:00:08 +0200

    OPP

--- P6Project/src/main/java/sw614f21/p6project/CODMiner/ClusterSymbol.java
+++ P6Project/src/main/java/sw614f21/p6project/CODMiner/ClusterSymbol.java
@@ -5,9 +5,11 @@ import sw614f21.p6project.TPMiner.PatternSymbol;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.text.DecimalFormat;
+import java.util.Collections;
 
 public class ClusterSymbol extends PatternSymbol {
     public NumberFormat doubleFormat = new DecimalFormat("###.##");
+    public ArrayList<Integer> symbolData;
     public double Mean;
     public double Deviation;
     public ArrayList<ClusterElement> ClusterElements;
@@ -22,8 +24,34 @@ public class ClusterSymbol extends PatternSymbol {
 
     }
     
+    
+    public double f (Integer x){
+        double h = h();
+        double constant = 1 / (symbolData.size() * h);
+        double sum = 0;
+        for (int i = 0; i < symbolData.size(); i++){
+            sum = sum + K(((x-symbolData.get(i)) / h));
+        }
+        return constant * sum;
+    }
+    
+    public double K(double x){
+        double constant = 1 / Math.sqrt(2 * Math.PI);
+        double exponent = Math.exp(-0.5 * Math.pow(x, 2));
+        return constant * exponent;
+    }
+    
+    public double h(){
+        Integer max = Collections.max(symbolData);
+        Integer min = Collections.min(symbolData);
+        return (max-min)/Math.sqrt(symbolData.size());
+    }
+    
+    
+    
     @Override
     public String toString() {
-        return EventID.concat((Start ? "+" : "-").concat(" Mean: ").concat(doubleFormat.format(Mean)).concat(" Deviation: ").concat(doubleFormat.format(Deviation)));
+        //return EventID.concat((Start ? "+" : "-").concat(" Mean: ").concat(doubleFormat.format(Mean)).concat(" Deviation: ").concat(doubleFormat.format(Deviation)));
+        return EventID.concat((Start ? "+" : "-"));
     }
 }
